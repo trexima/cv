16eddb7d088b2eda4eedfe09f92780bfefa82083
jbuncak <jan.buncak@protonmail.com>
2023-05-29 19:04:32 +0200

    Updated exporter (#15)
    Removed dompdf

--- src/Export/EuropeanCvExporter.php
+++ src/Export/EuropeanCvExporter.php
@@ -2,35 +2,37 @@
 
 namespace Trexima\EuropeanCvBundle\Export;
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
+use InvalidArgumentException;
 use Mpdf\Mpdf;
+use Mpdf\MpdfException;
+use Trexima\EuropeanCvBundle\Entity\Enum\StyleEnum;
 use Trexima\EuropeanCvBundle\Entity\EuropeanCV;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class EuropeanCvExporter
 {
-    final public const TYPE_PDF = 'pdf',
-        TYPE_DOC = 'doc';
+    final public const TYPE_PDF = 'pdf';
+    final public const TYPE_DOC = 'doc';
 
-    public function __construct(
-        private readonly string $projectDir,
-        private readonly string $uploadUrl,
-        private readonly Environment $twig
-    )
+    public function __construct(private readonly string $uploadUrl, private readonly Environment $twig)
     {
     }
 
     /**
-     * @return Mpdf
-     * @throws \Mpdf\MpdfException
+     * @throws MpdfException
      */
-    private function createMpdf()
+    private function createMpdf(array $config): Mpdf
     {
-        $mpdf = new Mpdf([
+        $config = array_merge([
             'format' => 'A4',
-            'tempDir' => sys_get_temp_dir().'/mpdf'
-        ]);
+            'tempDir' => sys_get_temp_dir() . '/mpdf',
+        ], $config);
+
+        $mpdf = new Mpdf($config);
+        $mpdf->use_kwt = true;
         $mpdf->shrink_tables_to_fit = 1;
         $mpdf->showImageErrors = true;
 
@@ -40,59 +42,58 @@ class EuropeanCvExporter
     /**
      * Generate HTML content for DOC or PDF.
      *
+     * @param EuropeanCV $europeanCV
      * @param string $exportType self::TYPE_PDF|self::TYPE_DOC
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws InvalidArgumentException When exportType is not supported
      */
-    public function generateContent(EuropeanCV $europeanCV, string $exportType)
+    public function generateContent(EuropeanCV $europeanCV, string $exportType): string
     {
-        return $this->twig->render('@TreximaEuropeanCv/export/european_cv.html.twig', [
+        if (!in_array($exportType, [self::TYPE_PDF, self::TYPE_DOC])) {
+            throw new InvalidArgumentException('Invalid export type provided');
+        }
+
+        $style = ($europeanCV->getStyle() ?? StyleEnum::STYLE_01)->value;
+
+        return $this->twig->render('@TreximaEuropeanCv/export/styles/' . $style . '.html.twig', [
             'exportType' => $exportType,
             'european_cv' => $europeanCV,
-            'image_upload_url' => ltrim($this->uploadUrl, '\\/').'/images/',
-            // PDF require absolute paths because mPDF needs to download images and connecting to foreign hosts(Cloudflare) is disabled
-            'img_use_absolute_path' => $exportType === self::TYPE_PDF
+            'image_upload_url' => rtrim($this->uploadUrl, '\\/') . '/images/',
+            // PDF requires absolute paths because mPDF needs to download images and connecting
+            // to foreign hosts (Cloudflare) is disabled
+            'img_use_absolute_path' => $exportType === self::TYPE_PDF,
         ]);
     }
 
     /**
      * Generate PDF or DOC content.
      *
+     * @param EuropeanCV $europeanCV
      * @param string $exportType self::TYPE_PDF|self::TYPE_DOC
+     * @param array $config Custom mPDF's configuration
      * @return string
-     * @throws \Mpdf\MpdfException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws InvalidArgumentException When exportType is not supported
+     * @throws MpdfException
      */
-    public function generate(EuropeanCV $europeanCV, string $exportType)
+    public function generate(EuropeanCV $europeanCV, string $exportType, array $config = []): string
     {
         $html = $this->generateContent($europeanCV, $exportType);
+
         switch ($exportType) {
             case self::TYPE_PDF:
-                // $mpdf = $this->createMpdf();
-                // $mpdf->WriteHTML($html);
-                // return $mpdf->Output('', 'S');
-                $dompdf = new Dompdf();
-                $dompdf->loadHtml($html);
-
-                $options = new Options();
-                $options->setChroot([$this->projectDir]);
-                $dompdf->setOptions($options);
-
-                // (Optional) Setup the paper size and orientation
-                $dompdf->setPaper('A4', 'portrait');
-
-                // Render the HTML as PDF
-                $dompdf->render();
-
-                // Output the generated PDF to Browser
-                return $dompdf->output();
-                break;
+                $mpdf = $this->createMpdf($config);
+                $mpdf->WriteHTML($html);
+                return $mpdf->Output('', 'S');
             case self::TYPE_DOC:
                 return $html;
-                break;
             default:
-                trigger_error('This output type isn\'t supported');
+                throw new InvalidArgumentException('Invalid export type provided');
         }
     }
 }
---
+++ src/Twig/SocialAccountRuntime.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Trexima\EuropeanCvBundle\Twig;
+
+use Twig\Extension\RuntimeExtensionInterface;
+
+class SocialAccountRuntime implements RuntimeExtensionInterface
+{
+    public const PATTERN_FACEBOOK = '/https?:\/\/(?:www\.)?facebook\.com\/([^\/?&]+)/i';
+    public const PATTERN_INSTAGRAM = '/https?:\/\/(?:www\.)?instagram\.com\/([^\/?&]+)/i';
+    public const PATTERN_LINKEDIN = '/https?:\/\/(?:www\.)?linkedin\.com\/in\/([^\/?&]+)/i';
+
+    /**
+     * Return a 'nick' from a social account
+     *
+     * @param string $url
+     * @return string
+     */
+    public function socialAccount(string $url): string
+    {
+        if (preg_match(self::PATTERN_FACEBOOK, $url, $matches)) {
+            $nick = urldecode($matches[1]);
+        } elseif (preg_match(self::PATTERN_INSTAGRAM, $url, $matches)) {
+            $nick = urldecode($matches[1]);
+        } elseif (preg_match(self::PATTERN_LINKEDIN, $url, $matches)) {
+            $nick = urldecode($matches[1]);
+        } else {
+            $nick = preg_replace('/^https?:\/\//i', '', $url);
+        }
+
+        return $nick;
+    }
+}
